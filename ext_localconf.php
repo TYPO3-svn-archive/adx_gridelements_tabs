00457ede6ff99904747dcd0036e7e94e9888f914
adgrafik <adgrafik@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-11-12 16:45:48 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/adx_gridelements_tabs/trunk@67795 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,12 +1,10 @@
 <?php
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
+// Register stdWrap hook for includeFrontEndResources. Using "adx*" key to prevent colliding of other adx-extensions using the same hook.
+$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_content.php']['stdWrap']['adxIncludeFrontEndResources'] = 'EXT:' . $_EXTKEY . '/Classes/Hooks/StdWrap.php:&Tx_AdxGridelementsTabs_Hooks_StdWrap';
+
 // Include TypoScript configuration.
 t3lib_extMgm::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TSconfig/Page.ts">');
-
-// Include stdWrap function for includeFrontEndResources.
-if (!t3lib_extMgm::isLoaded('ad_web_framework')) {
-	$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_content.php']['stdWrap'][] = 'EXT:' . $_EXTKEY . '/Classes/Hooks/StdWrap.php:&Tx_AdxGridelementsTabs_Hooks_StdWrap';
-}
 
 ?>
